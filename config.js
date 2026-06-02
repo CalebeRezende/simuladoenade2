377853a8970f0e758ef643459e75242a46467e69
CalebeRezende <120114655+CalebeRezende@users.noreply.github.com>
2026-06-02 12:19:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // CONFIGURAÇÃO DO SUPABASE
 // URL sem /rest/v1 no final.
 window.SUPABASE_URL = "https://sdaxxrphbvxroertnofnw.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_qB03yFr7MZg2va7FZZg0oA_wKf8rJHW";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYXh4cnBoYnZ4b2VydG5vZm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwODM2MDcsImV4cCI6MjA5NTY1OTYwN30.JwLMsggGst3qObUqwLnN-tun6V7hvGcuM9rvx9dFZGg";
