3e4e74b7114bc9f0965a84d18c6f5c6330b0c10b
CalebeRezende <120114655+CalebeRezende@users.noreply.github.com>
2026-06-02 11:57:56 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// CONFIGURAÇÃO DO SUPABASE
+// URL sem /rest/v1 no final.
+window.SUPABASE_URL = "https://sdaxxrphbvxroertnofnw.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_qB03yFr7MZg2va7FZZg0oA_wKf8rJHW";
