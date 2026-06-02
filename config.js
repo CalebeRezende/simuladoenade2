006cde20b8b3439727ad3d65001d7bac7e9a6ae5
CalebeRezende <120114655+CalebeRezende@users.noreply.github.com>
2026-06-02 13:53:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // CONFIGURAÇÃO DO SUPABASE
 window.SUPABASE_URL = "https://sdaxxrphbvxroertnofnw.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYXh4cnBoYnZ4b2VydG5vZm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwODM2MDcsImV4cCI6MjA5NTY1OTYwN30.JwLMsggGst3qObUqwLnN-tun6V7hvGcuM9rvx9dFZGg;
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYXh4cnBoYnZ4b2VydG5vZm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwODM2MDcsImV4cCI6MjA5NTY1OTYwN30.JwLMsggGst3qObUqwLnN-tun6V7hvGcuM9rvx9dFZGg";
 
 // Senha local do painel admin.
 // Troque antes de subir para o GitHub.
